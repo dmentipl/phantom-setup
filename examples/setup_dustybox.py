777a1ebdbcb7e467a9e6fff07254d6dbd002edfd
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-09-09 16:16:05 +1000

    Tidy up example setup script

--- examples/setup_dustybox.py
+++ examples/setup_dustybox.py
@@ -1,98 +1,128 @@
+"""
+Setup the DUSTYBOX test problem with multiple dust species.
+
+The dust and gas are co-located in a box. The gas is initially
+stationary, and the dust is given a uniform velocity to the right.
+"""
+
 import numpy as np
 import phantomsetup
 
-# ------------------------------------------------------------------------------------ #
-# Parameters
-hfact = 1.0
-prefix = 'test'
 
-# ------------------------------------------------------------------------------------ #
-# Instantiate phantomsetup object
-setup = phantomsetup.Setup()
-setup.prefix = 'test'
+def main():
 
-# ------------------------------------------------------------------------------------ #
-# Setup box
-setup.box = (-0.5, 0.5, -0.5, 0.5, -0.5, 0.5)
+    # -------------------------------------------------------------------------------- #
+    # Constants
+    igas = phantomsetup.defaults.igas
+    idust = phantomsetup.defaults.idust
 
-# ------------------------------------------------------------------------------------ #
-# Setup gas
-particle_type = phantomsetup.defaults.igas
-npartx = 32
-rho = 1.0
+    # -------------------------------------------------------------------------------- #
+    # Parameters
+    prefix = 'dustybox'
+    box_boundary = (-0.5, 0.5, -0.5, 0.5, -0.5, 0.5)
 
-# Particle positions
-particle_spacing = setup.box.xwidth / npartx
-position, smoothing_length = phantomsetup.dist.uniform_distribution(
-    boundary=setup.box.boundary, particle_spacing=particle_spacing, hfact=hfact
-)
-npart = position.shape[0]
-particle_mass = rho * setup.box.volume / npart
+    hfact = 1.0
 
-# Particle velocities
-velocity = np.zeros((npart, 3))
+    number_of_particles_in_x_gas = 32
+    number_of_particles_in_x_dust = 16
 
-# Add particles
-setup.add_particles(particle_type, particle_mass, position, velocity, smoothing_length)
+    density_gas = 1.0
+    density_dust = (0.01, 0.02, 0.03, 0.04, 0.05)
 
-# ------------------------------------------------------------------------------------ #
-# Setup dust
-particle_type = phantomsetup.defaults.idust
-npartx = 16
-rho = 0.01
+    velocity_x_gas = 0.0
+    velocity_x_dust = 1.0
 
-# Particle positions
-particle_spacing = setup.box.xwidth / npartx
-position, smoothing_length = phantomsetup.dist.uniform_distribution(
-    boundary=setup.box.boundary, particle_spacing=particle_spacing, hfact=hfact
-)
-npart = position.shape[0]
-particle_mass = rho * setup.box.volume / npart
+    # -------------------------------------------------------------------------------- #
+    # Instantiate phantomsetup object
+    dustybox = phantomsetup.Setup()
+    dustybox.prefix = prefix
 
-# Particle velocities
-velocity = np.hstack((np.ones((npart, 1)), np.zeros((npart, 2))))
+    # -------------------------------------------------------------------------------- #
+    # Setup box
+    dustybox.box = box_boundary
 
-# Add particles
-setup.add_particles(particle_type, particle_mass, position, velocity, smoothing_length)
+    # -------------------------------------------------------------------------------- #
+    # Setup particles
 
-# ------------------------------------------------------------------------------------ #
-# TODO: add other arrays...
-alpha = np.zeros(setup.total_number_of_particles)
-setup.add_array_to_particles('alpha', alpha)
+    number_of_dust_species = len(density_dust)
+    dust_types = tuple(range(idust, idust + number_of_dust_species))
 
-# ------------------------------------------------------------------------------------ #
-# TODO: add other header items...
+    particle_types = (igas, *dust_types)
 
-# ------------------------------------------------------------------------------------ #
-# Write dump
-setup.write_dump_file()
+    npartx = {igas: number_of_particles_in_x_gas}
+    npartx.update({idx: number_of_particles_in_x_dust for idx in dust_types})
 
-# ------------------------------------------------------------------------------------ #
-# DEBUGGING
-if False:
+    rho = {igas: density_gas}
+    rho.update(dict(zip(dust_types, density_dust)))
 
-    import matplotlib.pyplot as plt
-    from IPython import get_ipython
+    velx = {igas: velocity_x_gas}
+    velx.update({idx: velocity_x_dust for idx in dust_types})
 
-    ipython = get_ipython()
-    ipython.magic('matplotlib')
+    for itype in particle_types:
+        add_particle_of_type_to_box(
+            dustybox, itype, npartx[itype], rho[itype], velx[itype], hfact
+        )
 
-    fig, axes = plt.subplots(1, 3)
+    # -------------------------------------------------------------------------------- #
+    # TODO: add other arrays...
+    alpha = np.zeros(dustybox.total_number_of_particles)
+    dustybox.add_array_to_particles('alpha', alpha)
 
-    axes[0].plot(position[:, 0], position[:, 1], '.')
-    axes[0].set_aspect('equal')
-    axes[0].set_xlabel('x')
-    axes[0].set_ylabel('y')
+    # -------------------------------------------------------------------------------- #
+    # TODO: add other header items...
 
-    axes[1].plot(position[:, 0], position[:, 2], '.')
-    axes[1].set_aspect('equal')
-    axes[1].set_xlabel('x')
-    axes[1].set_ylabel('z')
+    # -------------------------------------------------------------------------------- #
+    # Write dump and in file
+    dustybox.write_dump_file()
+    dustybox.write_in_file()
 
-    axes[2].plot(position[:, 1], position[:, 2], '.')
-    axes[2].set_aspect('equal')
-    axes[2].set_xlabel('y')
-    axes[2].set_ylabel('z')
 
-    plt.tight_layout()
-    plt.show()
+def add_particle_of_type_to_box(
+    setup: phantomsetup.Setup,
+    particle_type: int,
+    npartx: int,
+    rho: float,
+    velx: float,
+    hfact: float,
+) -> None:
+    """
+    Add particles of a particular type to the box.
+
+    Parameters
+    ----------
+    setup : phantomsetup.Setup
+        The phantomsetup object representing the simulation.
+    particle_type: int
+        The particle type to add.
+    npartx: int
+        The number of particles in the x-direction. Thus the total
+        number of particles for a cube box would be roughly npartx**3.
+    rho: float
+        The initial density.
+    velx: float
+        The initial uniform velocity in the x-direction.
+    hfact: float
+        The smoothing length factor 'hfact'.
+    """
+
+    # Particle positions
+    particle_spacing = setup.box.xwidth / npartx
+    position, smoothing_length = phantomsetup.dist.uniform_distribution(
+        boundary=setup.box.boundary, particle_spacing=particle_spacing, hfact=hfact
+    )
+    npart = position.shape[0]
+
+    # Particle mass
+    particle_mass = rho * setup.box.volume / npart
+
+    # Particle velocities
+    velocity = np.hstack((velx * np.ones((npart, 1)), np.zeros((npart, 2))))
+
+    # Add particles
+    setup.add_particles(
+        particle_type, particle_mass, position, velocity, smoothing_length
+    )
+
+
+if __name__ == '__main__':
+    main()
