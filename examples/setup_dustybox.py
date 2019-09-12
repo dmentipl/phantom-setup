718ac119cfa1e8eccb55676e5351604b5eb21e45
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-09-12 18:12:51 +1000

    Expand on dustybox example setup script
    Plus add a TOML file for the parameters.

--- examples/setup_dustybox.py
+++ examples/setup_dustybox.py
@@ -1,7 +1,7 @@
 """
 Setup the DUSTYBOX test problem.
 
-The dust, with possibly multiple species, and gas are co-located in a
+The dust, possibly with multiple species, and gas are co-located in a
 box. The gas is initially stationary, and the dust is given a uniform
 velocity to the right.
 
@@ -22,117 +22,167 @@ particles, add additional arrays, write a 'temporary' dump file (the
 Phantom initial condition), and write a Phantom 'in' file.
 """
 
+import argparse
+import dataclasses
+import pathlib
+from dataclasses import dataclass
+from pathlib import Path
 from typing import Collection, Union
 
 import numpy as np
 import phantomsetup
+import toml
 
-# TODO: things to set that are not currently set
-# - units (if using physical drag, i.e. Epstein/Stokes)
-# - grain_size and grain_density (if using physical drag, i.e. Epstein/Stokes)
-
-# -------------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------------------ #
 # Constants
 
+# igas and idust are Phantom integer types for particles
 igas = phantomsetup.defaults.particle_type['igas']
 idust = phantomsetup.defaults.particle_type['idust']
 
+# constant values are in cgs
 au = phantomsetup.constants.au
 solarm = phantomsetup.constants.solarm
 year = phantomsetup.constants.year / (2 * np.pi)
 
+# hfact=1.0 is for the quintic kernel which is best practice for
+# calculations with dust
+hfact = 1.0
 
-def main():
+# ieos=1 sets the isothermal equation of state
+ieos = 1
 
-    # -------------------------------------------------------------------------------- #
-    # Parameters
 
-    prefix = 'dustybox'
+# ------------------------------------------------------------------------------------ #
+# Parameters
 
-    box_boundary = (-0.5, 0.5, -0.5, 0.5, -0.5, 0.5)
 
-    hfact = 1.0
-    ieos = 1
-    sound_speed = 1.0
+@dataclass
+class Parameters:
+    """DUSTYBOX setup parameters."""
 
-    number_of_particles_in_x_gas = 32
-    number_of_particles_in_x_dust = 16
+    prefix: str = 'dustybox'
 
-    density_gas = 1.0
-    dust_to_gas_ratio = (0.01, 0.02, 0.03, 0.04, 0.05)
+    box_boundary: tuple = (-0.5, 0.5, -0.5, 0.5, -0.5, 0.5)
 
-    drag_method = 'K_const'
+    sound_speed: float = 1.0
 
-    K_code = 1.0
+    number_of_particles_in_x_gas: int = 32
+    number_of_particles_in_x_dust: int = 16
 
-    velocity_x_gas = 0.0
-    velocity_x_dust = 1.0
+    density_gas: float = 1.0
+    dust_to_gas_ratio: tuple = (0.01, 0.02, 0.03, 0.04, 0.05)
+
+    drag_method: str = 'K_const'
+    K_code: float = 1.0
+    grain_size: tuple = ()
+    grain_density: float = 0.0
+
+    velocity_x_gas: float = 0.0
+    velocity_x_dust: float = 1.0
+
+
+# ------------------------------------------------------------------------------------ #
+# Setup function for DUSTYBOX
+
+
+def setup_dustybox(parameters: Parameters) -> phantomsetup.Setup:
+    """
+    Setup DUSTYBOX.
+
+    This function sets up the DUSTYBOX test problem. It reads in
+    parameters, instantiates the phantomsetup.Setup object, and adds
+    particles and other information, then writes a Phantom initial
+    condition dump and Phantom in file.
+
+    Parameters
+    ----------
+    parameters : Parameters
+        The parameters for this particular problem.
+
+    Returns
+    -------
+    setup : phantomsetup.Setup
+        The phantomsetup.Setup object.
+    """
 
     # -------------------------------------------------------------------------------- #
     # Instantiate phantomsetup object
 
-    dustybox = phantomsetup.Setup()
+    setup = phantomsetup.Setup()
 
     # -------------------------------------------------------------------------------- #
     # File prefix
 
-    dustybox.prefix = prefix
+    setup.prefix = parameters.prefix
 
     # -------------------------------------------------------------------------------- #
     # Set unit
 
-    dustybox.set_units(au, solarm, year)
+    setup.set_units(au, solarm, year)
 
     # -------------------------------------------------------------------------------- #
     # Set equation of state
 
-    dustybox.set_equation_of_state(ieos=ieos, polyk=sound_speed ** 2)
+    setup.set_equation_of_state(ieos=ieos, polyk=parameters.sound_speed ** 2)
 
     # -------------------------------------------------------------------------------- #
     # Set dust grain size distribution
 
-    dustybox.set_dust(
-        dust_method='largegrains',
-        drag_method=drag_method,
-        drag_constant=K_code,
-        number_of_dust_species=len(dust_to_gas_ratio),
-    )
+    if parameters.drag_method == 'Epstein/Stokes':
+        setup.set_dust(
+            dust_method='largegrains',
+            drag_method=parameters.drag_method,
+            grain_size=parameters.grain_size,
+            grain_density=parameters.grain_density,
+        )
+
+    elif parameters.drag_method in ('K_const', 'ts_const'):
+        setup.set_dust(
+            dust_method='largegrains',
+            drag_method=parameters.drag_method,
+            drag_constant=parameters.K_code,
+            number_of_dust_species=len(parameters.dust_to_gas_ratio),
+        )
 
     # -------------------------------------------------------------------------------- #
     # Setup box
 
-    dustybox.set_boundary(box_boundary)
+    setup.set_boundary(parameters.box_boundary)
 
     # -------------------------------------------------------------------------------- #
     # Add dust and gas particles to box
 
     add_particles_to_box(
-        dustybox,
-        number_of_particles_in_x_gas,
-        number_of_particles_in_x_dust,
-        density_gas,
-        dust_to_gas_ratio,
-        velocity_x_gas,
-        velocity_x_dust,
-        hfact,
+        setup,
+        parameters.number_of_particles_in_x_gas,
+        parameters.number_of_particles_in_x_dust,
+        parameters.density_gas,
+        parameters.dust_to_gas_ratio,
+        parameters.velocity_x_gas,
+        parameters.velocity_x_dust,
     )
 
     # -------------------------------------------------------------------------------- #
     # Add extra quantities to particles
 
-    alpha = np.zeros(dustybox.total_number_of_particles, dtype=np.single)
-    dustybox.add_array_to_particles('alpha', alpha)
+    alpha = np.zeros(setup.total_number_of_particles, dtype=np.single)
+    setup.add_array_to_particles('alpha', alpha)
 
     # -------------------------------------------------------------------------------- #
     # Write dump and in file
 
-    dustybox.write_dump_file()
-    dustybox.write_in_file()
+    setup.write_dump_file()
+    setup.write_in_file()
 
     # -------------------------------------------------------------------------------- #
     # Return the phantomsetup.Setup object
 
-    return dustybox
+    return setup
+
+
+# ------------------------------------------------------------------------------------ #
+# Helper functions
 
 
 def add_particles_to_box(
@@ -143,8 +193,7 @@ def add_particles_to_box(
     dust_to_gas_ratio: Union[Collection, np.ndarray],
     velocity_x_gas: float,
     velocity_x_dust: float,
-    hfact: float,
-):
+) -> None:
     """
     Helper function to add particles to the box.
 
@@ -166,8 +215,6 @@ def add_particles_to_box(
         The initial uniform gas velocity in the x-direction.
     velocity_x_gas: float
         The initial uniform dust velocity in the x-direction.
-    hfact: float
-        The smoothing length factor 'hfact'.
     """
 
     number_of_dust_species = len(dust_to_gas_ratio)
@@ -187,17 +234,14 @@ def add_particles_to_box(
 
     for itype in particle_types:
         _add_particle_of_type_to_box(
-            setup, itype, npartx[itype], rho[itype], velx[itype], hfact
+            setup, itype, npartx[itype], rho[itype], velx[itype]
         )
+
+    return
 
 
 def _add_particle_of_type_to_box(
-    setup: phantomsetup.Setup,
-    particle_type: int,
-    npartx: int,
-    rho: float,
-    velx: float,
-    hfact: float,
+    setup: phantomsetup.Setup, particle_type: int, npartx: int, rho: float, velx: float
 ) -> None:
     """
     Helper function to add particles of a particular type to the box.
@@ -215,8 +259,6 @@ def _add_particle_of_type_to_box(
         The initial density.
     velx: float
         The initial uniform velocity in the x-direction.
-    hfact: float
-        The smoothing length factor 'hfact'.
     """
 
     # Particle positions
@@ -237,6 +279,71 @@ def _add_particle_of_type_to_box(
         particle_type, particle_mass, position, velocity, smoothing_length
     )
 
+    return
+
+
+def read_parameters_from_file(filename: Union[str, Path]) -> Parameters:
+    """
+    Read parameters from TOML config file.
+
+    Parameters
+    ----------
+    filename : str or Path
+        The file name or path to the file to read.
+
+    Returns
+    -------
+    Parameters
+        The parameters from the file as a Parameters dataclass object.
+    """
+
+    filepath = pathlib.Path(filename).expanduser().resolve()
+    t = toml.load(filepath)
+    d = dict()
+    for key, val in t.items():
+        if isinstance(val, list):
+            d[key] = tuple(val)
+        else:
+            d[key] = val
+    return Parameters(**t)
+
+
+def write_parameters_to_file(
+    *, parameters: Parameters, filename: Union[str, Path]
+) -> None:
+    """
+    Read parameters from TOML config file.
+
+    Note: this does not preserve comments.
+
+    Parameters
+    ----------
+    parameters : Parameters
+        The parameters as a Parameters dataclass.
+    filename : str or Path
+        The file name or path to the file to read.
+    """
+    with open(filename, 'w') as fp:
+        toml.dump(dataclasses.asdict(parameters), fp)
+    return
+
+
+# ------------------------------------------------------------------------------------ #
+# Entry point for setup script
 
 if __name__ == '__main__':
-    dustybox = main()
+
+    parser = argparse.ArgumentParser(description='Setup DUSTYBOX.')
+    parser.add_argument('filename', type=Path, help='the setup parameter file')
+    args = parser.parse_args()
+    filename = args.filename
+
+    if not filename.exists():
+        print(f'No file named "{filename}" exists')
+        if filename.suffix != '.toml':
+            filename = filename.with_suffix('.toml')
+        print(f'Writing default parameter file: {filename}')
+        write_parameters_to_file(parameters=Parameters(), filename=filename)
+    else:
+        parameters = read_parameters_from_file(filename)
+        setup = setup_dustybox(parameters)
