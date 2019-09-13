67783a7acb99885eb3c808e315b2231f45cc7bdf
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-09-13 10:13:52 +1000

    Add tomlkit as requirement, remove toml file

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
     description='Phantom setup with Python and HDF5',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['phantomconfig'],
+    install_requires=['phantomconfig', 'tomlkit'],
 )
